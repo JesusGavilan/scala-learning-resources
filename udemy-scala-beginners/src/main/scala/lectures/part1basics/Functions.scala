0af430fcc3a02fd1cff78a25c8d8203f2b860339
JesusGavilan <jegavilgar@gmail.com>
2020-04-24 01:20:58 +0200

    Updated functions lesson

--- udemy-scala-beginners/src/main/scala/lectures/part1basics/Functions.scala
+++ udemy-scala-beginners/src/main/scala/lectures/part1basics/Functions.scala
@@ -34,12 +34,18 @@ object Functions extends App {
   def greeting(name: String, age: Int): Unit = {
     println(s"Hi, my name is $name and I am $age years old")
   }
+  greeting("Jesus", 30)
   // 2. Factorial function 1 * 2 * 3  * .... * n
-
   def factorial(n: Int): Int = {
     if (n == 1) n else factorial(n-1)*n
   }
-  println(factorial(1))
+  println(factorial(4))
   // 3. A Fibonacci function: f(1) = 1, f(2) = 1, f(n) = f(n-1) + f(n-2)
-  //4. TEst if a number is prime
+
+  def fibonacci(n: Int): Int = {
+    if (n > 0 & n < 3) 1 else fibonacci(n-1) + fibonacci(n-2)
+  }
+  println(fibonacci(14))
+  //4. Test if a number is prime
+
 }
