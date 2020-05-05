ad9ae0b413629be275aa42fc5f02d080386623c0
JesusGavilan <jegavilgar@gmail.com>
2020-05-06 01:36:54 +0200

    Added recursirve tail exercise

--- udemy-scala-beginners/src/main/scala/lectures/part1basics/Recursion.scala
+++ udemy-scala-beginners/src/main/scala/lectures/part1basics/Recursion.scala
@@ -29,13 +29,35 @@ object Recursion extends App {
 
   //Execises:
   //1. Concatenate a string n times using tail recursion
+  @tailrec
+  def concatTailRec(x: Int,  aString: String, aStringAccumulator: String): String =
+    if (x <= 0) aStringAccumulator
+    else concatTailRec(x-1, aString, aString + aStringAccumulator)
 
+  println(concatTailRec(5, "hola|", ""))
 
   //2. IsPrime function tail recursive
-
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def isPrimeTailRec(t: Int, isStillPrime: Boolean): Boolean =
+      if (!isStillPrime) false
+      else if (t <= 1) true
+      else isPrimeTailRec(t-1, n % t!=0 && isStillPrime)
+    isPrimeTailRec(n/2, true)
+  }
+  println(isPrime(2003))
+  println(isPrime(629))
 
   //3. Fibonacci function, tail recursive
-
+  def fibonacci(n: Int): Int = {
+    @tailrec
+    def fibonacciTailRec(x: Int, accumulator1: Int, accumulator2: Int): Int =
+      if (x > n) accumulator1
+      else fibonacciTailRec(x+1, accumulator1 + accumulator2, accumulator1)
+    if (n <= 2) 1
+    fibonacciTailRec(3, 1, 1)
+  }
+  println(fibonacci(10))
 
 
 }
