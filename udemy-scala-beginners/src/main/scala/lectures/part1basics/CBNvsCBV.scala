8b36105ce3a524119e53db0a82bc24d8bb7e120f
JesusGavilan <jegavilgar@gmail.com>
2020-05-25 17:51:52 +0200

    added call by name or call by value examples

---
+++ udemy-scala-beginners/src/main/scala/lectures/part1basics/CBNvsCBV.scala
@@ -0,0 +1,21 @@
+package lectures.part1basics
+
+object CBNvsCBV extends App {
+  def calledByValue(x: Long): Unit = {
+    println("by value: "  + x)
+    println("by value: " +  x)
+  }
+  def calledByName(x: => Long): Unit = {
+    println("by value: " + x)
+    println("by value: " + x)
+  }
+
+  calledByValue(System.nanoTime())
+  calledByName(System.nanoTime())
+
+  def infinite(): Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int) = println(x)
+
+  printFirst(34, infinite()) //lazy evaluation wich infinite() is never eval, only the first one
+
+}
