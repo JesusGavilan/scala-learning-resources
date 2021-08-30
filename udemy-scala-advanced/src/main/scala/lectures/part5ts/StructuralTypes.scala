eb7783d5f1be7be35086b99668e9ee6d56ff1b3d
JesusGavilan <jegavilgar@gmail.com>
2021-08-30 18:38:45 +0200

    Added the structural type lesson (partially)

---
+++ udemy-scala-advanced/src/main/scala/lectures/part5ts/StructuralTypes.scala
@@ -0,0 +1,22 @@
+package lectures.part5ts
+
+object StructuralTypes extends App {
+  // structural types
+
+  type JavaCloseable = java.io.Closeable
+
+  class HipsterCloseable {
+    def close(): Unit = println("yeah yeah I'm closing")
+  }
+
+  //def closeQuietly(closeable: JavaCloseable OR HipsterCloseable) // ???!
+
+  // STRUCTURAL TYPE
+  type UnifiedCloseable = {
+    def close(): Unit
+  }
+
+  def closeQuietly(unifiedCloseable: UnifiedCloseable): Unit = unifiedCloseable.close()
+
+  closeQ
+}
